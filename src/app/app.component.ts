bd622eb86291461c6b3723dde92cd07117366d16
Emerson Vergara <ee-web@hotmail.com>
2022-02-22 20:04:16 -0500

    primera clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myApp';
+  title: string = 'Esto es angular';
+  name: string = 'Nicolas';
+  rockBandName: string;
+
+  rockBands = ['Nirvana', 'Los prisoneros', 'Cass', 'as'];
+
+  constructor(){
+    this.rockBandName = '';
+  }
+
+  changeTittle(): void {
+    this.title = "Aprendiendo Angular";
+    this.changeName();
+  }
+
+  private changeName(): void {
+    this.name = "Nombre cambiado";
+  }
+
+  private cleanRockBandName(): void {
+    this.rockBandName = '';
+  }
+
+  addRockBand(): void {
+    if(this.rockBandName != ''){
+      this.rockBands.push(this.rockBandName);
+      this.cleanRockBandName();
+    }
+  }
 }
